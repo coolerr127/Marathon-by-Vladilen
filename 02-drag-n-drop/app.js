f7e513300f63db547b26f7a75e8244bb87497d9f
Anton Shimanovich <ashimanovich127@gmail.com>
2021-06-17 10:59:11 +0300

    done the second task

---
+++ 02-drag-n-drop/app.js
@@ -0,0 +1,38 @@
+const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
+
+item.addEventListener('dragstart', dragstart);
+item.addEventListener('dragend', dragend);
+
+placeholders.forEach((item) => {
+    item.addEventListener('dragover', dragover);
+    item.addEventListener('dragenter', dragenter);
+    item.addEventListener('dragleave', dragleave);
+    item.addEventListener('drop', dragdrop);
+});
+
+function dragstart(event) {
+    event.target.classList.add('hold');
+    setTimeout(() => event.target.classList.add('hide'), 0);
+}
+
+function dragend(event) {
+    event.target.classList.remove('hold', 'hide');
+}
+
+function dragover(event) {
+    event.preventDefault();
+}
+
+function dragenter(event) {
+    event.target.classList.add('hovered');
+}
+
+function dragleave(event) {
+    event.target.classList.remove('hovered');
+}
+
+function dragdrop(event) {
+    event.target.append(item);
+    event.target.classList.remove('hovered');
+}
